7a58815c67e14241e0fbc45ad06278a6c4f2fbfd
jussara-chicoma <chicoma.borges@discente.ufg.br>
2026-05-18 20:21:26 -0300

    Add files via upload

---
+++ database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Conectar() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	var err error
+	DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Erro ao abrir conexao: %v", err)
+	}
+	if err = DB.Ping(); err != nil {
+		log.Fatalf("Erro ao conectar ao PostgreSQL: %v", err)
+	}
+
+	log.Println("PostgreSQL conectado com sucesso!")
+}
